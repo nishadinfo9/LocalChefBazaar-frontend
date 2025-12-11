57e4c071ebbabbcd03b1b37a4b2176e7b5f52dbf
nishadinfo09 <mdnishadali03@gmail.com>
2025-12-11 14:23:37 +0600

    implement pagination

--- src/pages/Meals/Meals.jsx
+++ src/pages/Meals/Meals.jsx
@@ -3,20 +3,34 @@ import FoodCard from "../Home/FoodCard/FoodCard";
 import useFetch from "../../hooks/useFetch";
 import Loader from "../../utils/Loader";
 import SortSelect from "./SortSelect";
+import Pagination from "./Pagination";
 
 const Meals = () => {
+  const [currentPage, setCurrentPage] = useState(0);
   const [sortVal, setSortVal] = useState("");
-  const { data, isLoading, isError, error } = useFetch({
-    url: `/meals/all-meals?order=${sortVal}`,
-    queryKey: ["meals", sortVal],
+  let limit = 10;
+
+  const { data, isLoading, isError, error, refetch } = useFetch({
+    url: `/meals/all-meals?limit=${limit}&skip=${
+      currentPage * limit
+    }&order=${sortVal}`,
+    queryKey: ["meals", sortVal, currentPage],
+    enabled: true,
+    keepPreviousData: true,
+    staleTime: 1000 * 60 * 3,
   });
 
   const sortHandler = (value) => {
     setSortVal(value);
+    setCurrentPage(0);
   };
 
   if (isLoading) return <Loader />;
   if (isError) return <p>{error}</p>;
+
+  const totalCount = data?.total || 0;
+  const meals = data?.meals || [];
+  const totalPage = Math.ceil(totalCount / limit);
 
   return (
     <div className="my-10">
@@ -29,10 +43,17 @@ const Meals = () => {
           <SortSelect sortVal={sortVal} sortHandler={sortHandler} />
         </div>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-10 md:gap-20">
-          {data?.meals?.map((meal) => (
+          {meals?.map((meal) => (
             <FoodCard key={meal._id} meal={meal} />
           ))}
         </div>
+      </div>
+      <div>
+        <Pagination
+          totalPage={totalPage}
+          currentPage={currentPage}
+          setCurrentPage={setCurrentPage}
+        />
       </div>
     </div>
   );
---
+++ src/pages/Meals/Pagination.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Pagination = ({ currentPage, totalPage, setCurrentPage }) => {
+  return (
+    <div className="flex items-center justify-center my-10 gap-2">
+      <button className=" btn">Previous</button>
+      {[...Array(totalPage).keys()].map((page) => (
+        <button
+          key={page}
+          className={`btn rounded-lg ${currentPage === page && "bg-blue-500"}`}
+          onClick={() => setCurrentPage(page)}
+        >
+          {page + 1}
+        </button>
+      ))}
+      <button className="btn">Next</button>
+    </div>
+  );
+};
+
+export default Pagination;
