6e8cf5235a72a7d2640a2e48df750f54f0f3108d
nishadinfo09 <mdnishadali03@gmail.com>
2025-12-12 01:43:18 +0600

    add api url

--- src/hooks/useApi.jsx
+++ src/hooks/useApi.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const useApi = () => {
   const api = axios.create({
-    baseURL: "http://localhost:3000/api/v1",
+    baseURL: "https://localchefbazaar-backend-production.up.railway.app/api/v1",
     withCredentials: true,
   });
 
