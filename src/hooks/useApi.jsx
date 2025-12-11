3c9410ce157690b2d91b7f2330f3544a7f8bb27c
nishadinfo09 <mdnishadali03@gmail.com>
2025-12-11 19:13:49 +0600

    update API base URL and remove unused error handling code

--- src/hooks/useApi.jsx
+++ src/hooks/useApi.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 import axios from "axios";
-import toast from "react-hot-toast";
 
 const useApi = () => {
   const api = axios.create({
-    //https://localchefbazaar-backend-production.up.railway.app/api/v1
-    baseURL: "http://localhost:3000/api/v1",
+    baseURL: "https://localchefbazaar-backend-production.up.railway.app/api/v1",
     withCredentials: true,
   });
 
@@ -14,18 +12,6 @@ const useApi = () => {
     async (err) => {
       const originalRequest = err.config;
       const status = err.response?.status;
-      const message = err.response?.data?.message;
-
-      //handle fraud
-      // if (status === 403 && message?.includes("banned")) {
-      //   toast.error("⚠️ You account has banned.");
-      //   return Promise.reject(err);
-      // }
-
-      // if (status === 403 && message?.includes("Access denied")) {
-      //   toast.error("⛔ You do not have permission to access this page.");
-      //   return Promise.reject(err);
-      // }
 
       if (status === 401 && !originalRequest._retry) {
         originalRequest._retry = true;
